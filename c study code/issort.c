3acae2e2e7fd90111979c45e68625edb5326cd4f
faterer <faterer@gmail.com>
2012-11-18 15:11:40 +0800

    insert sort
    learn from mastering algorithms with C

---
+++ c study code/issort.c
@@ -0,0 +1,53 @@
+/* issort.c */
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int issort(void *data, int size, int esize, int (*compare)(const void *key1, const void *key2))
+{
+	char *a = data;
+	void *key;
+	int  i,j;
+	
+	if ((key = (char*)malloc(esize)) == NULL)
+	return -1;
+	
+	for (j = 1; j < size; j++)
+	{
+		memcpy(key, &a[j * esize], esize);
+		i = j - 1;
+		
+		while(i >=0 && compare(&a[i * esize], key) > 0)
+		{
+			memcpy(&a[(i + 1) * esize], &a[i * esize], esize);
+			i--;
+		}
+		
+		memcpy(&a[(i + 1) * esize], key, esize);
+	}
+	
+	free(key);
+	
+	return 0;
+}
+
+int compare_int(const void *key1, const void *key2)
+{
+	if (*(int *)key1 > *(int *)key2)
+		return 1;
+	else
+		return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int a[5] = {4,3,1,2,5};
+	int i;
+	
+	issort(a, sizeof(a)/sizeof(int), sizeof(int), compare_int);
+	
+	for (i = 0; i < 5; i++)
+	printf("%d\n", a[i]);
+	
+	return 0;
+}
