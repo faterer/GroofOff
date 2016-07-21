cf0586f3ec254064b35cb630e87707a632ad4a43
Mai Wang <mai.wang@nokia.com>
2016-07-21 17:42:11 +0800

    u

--- c/string/boyer_moore_3.c
+++ c/string/boyer_moore_3.c
@@ -7,14 +7,13 @@
 #define NOT_FOUND patlen
 #define max(a, b) ((a < b) ? b : a)
 
-// delta1 table: delta1[c] contains the distance between the last
-// character of pat and the rightmost occurrence of c in pat.
-// If c does not occur in pat, then delta1[c] = patlen.
-// If c is at string[i] and c != pat[patlen-1], we can
-// safely shift i over by delta1[c], which is the minimum distance
-// needed to shift pat forward to get string[i] lined up 
-// with some character in pat.
-// this algorithm runs in alphabet_len+patlen time.
+#define PRINT_TABLE(table, len) \
+do {\
+    for(int i = 0; i < len; i++)\
+    printf("%d,", table[i]);\
+    printf("\n");\
+}while(0)
+
 void make_delta1(int *delta1, uint8_t *pat, int32_t patlen) {
     int i;
     for (i = 0; i < ALPHABET_LEN; i++) {
@@ -49,41 +48,6 @@ int suffix_length(uint8_t *word, int wordlen, int pos) {
     return i;
 }
 
-// delta2 table: given a mismatch at pat[pos], we want to align 
-// with the next possible full match could be based on what we
-// know about pat[pos+1] to pat[patlen-1].
-//
-// In case 1:
-// pat[pos+1] to pat[patlen-1] does not occur elsewhere in pat,
-// the next plausible match starts at or after the mismatch.
-// If, within the substring pat[pos+1 .. patlen-1], lies a prefix
-// of pat, the next plausible match is here (if there are multiple
-// prefixes in the substring, pick the longest). Otherwise, the
-// next plausible match starts past the character aligned with 
-// pat[patlen-1].
-// 
-// In case 2:
-// pat[pos+1] to pat[patlen-1] does occur elsewhere in pat. The
-// mismatch tells us that we are not looking at the end of a match.
-// We may, however, be looking at the middle of a match.
-// 
-// The first loop, which takes care of case 1, is analogous to
-// the KMP table, adapted for a 'backwards' scan order with the
-// additional restriction that the substrings it considers as 
-// potential prefixes are all suffixes. In the worst case scenario
-// pat consists of the same letter repeated, so every suffix is
-// a prefix. This loop alone is not sufficient, however:
-// Suppose that pat is "ABYXCDBYX", and text is ".....ABYXCDEYX".
-// We will match X, Y, and find B != E. There is no prefix of pat
-// in the suffix "YX", so the first loop tells us to skip forward
-// by 9 characters.
-// Although superficially similar to the KMP table, the KMP table
-// relies on information about the beginning of the partial match
-// that the BM algorithm does not have.
-//
-// The second loop addresses case 2. Since suffix_length may not be
-// unique, we want to take the minimum value, which will tell us
-// how far away the closest potential match is.
 void make_delta2(int *delta2, uint8_t *pat, int32_t patlen) {
     int p;
     int last_prefix_index = patlen - 1;
@@ -92,20 +56,18 @@ void make_delta2(int *delta2, uint8_t *pat, int32_t patlen) {
     for (p = patlen - 1; p >= 0; p--) {
         if (is_prefix(pat, patlen, p + 1)) {
             last_prefix_index = p + 1;
-	    printf("%d:%d\n", p, last_prefix_index);
         }
         delta2[p] = last_prefix_index + patlen - 1 - p;
     }
-    for(int i = 0; i < patlen; i++)
-    printf("%d,", delta2[i]);
-    printf("\n");
+    PRINT_TABLE(delta2, patlen);
     // second loop
-    for (p=0; p < patlen - 1; p++) {
+    for (p = 0; p < patlen - 1; p++) {
         int slen = suffix_length(pat, patlen, p);
         if (pat[p - slen] != pat[patlen - 1 - slen]) {
             delta2[patlen - 1 - slen] = patlen - 1 - p + slen;
         }
     }
+    PRINT_TABLE(delta2, patlen);
 }
 
 uint8_t* boyer_moore (uint8_t *string, uint32_t stringlen, uint8_t *pat, uint32_t patlen) {
@@ -114,25 +76,25 @@ uint8_t* boyer_moore (uint8_t *string, uint32_t stringlen, uint8_t *pat, uint32_
     int *delta2 = (int *)malloc(patlen * sizeof(int));
     make_delta1(delta1, pat, patlen);
     make_delta2(delta2, pat, patlen);
-    //for(i = 0; i < patlen; i++)
-    //printf("%d,", delta2[i]);
-    //printf("\n");
+
     // The empty pattern must be considered specially
     if (patlen == 0) return string;
 
     j = 0;
     while (j <= stringlen - patlen) {
+        printf("%s\n", string);
+        for(int s = 0; s < j; s++) printf(" ");
+        printf("%s\n", pat);
         for (i = patlen - 1; i >= 0 && string[j+i] == pat[i]; --i);
         if (i < 0) {
             free(delta2);
             return (string + j);
         }
         else {
-            printf("Gs=%d\n", delta2[i] - patlen + 1 + i);
-            printf("Bc=%d\n", delta1[string[j+i]] - patlen + 1 + i);
+            printf("GsMoveStep=%d\n", delta2[i] - patlen + 1 + i);
+            printf("BcMoveStep=%d\n", delta1[string[j+i]] - patlen + 1 + i);
             int k = max(delta1[string[j+i]] - patlen + 1 + i, delta2[i] - patlen + 1 + i);
             j += k;
-            printf("%d\n", k);
         }
     }
     free(delta2);
