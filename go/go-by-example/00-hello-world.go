caed915ea924d2d9f07757a305ea3237224c4cbc
Bybo <faterer@gmail.com>
2018-06-21 23:05:15 +0800

    u

---
+++ go/go-by-example/00-hello-world.go
@@ -0,0 +1,8 @@
+//go run 00-hello-world.go
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world!")
+}
---
+++ go/go-by-example/01-values.go
@@ -0,0 +1,17 @@
+//go run 01-values.go
+package main
+
+import "fmt"
+
+//Go has various value types including strings, integers, floats, booleans, etc. Here are a few basic examples.
+//Strings, which can be added together with +.
+//Integers and floats.
+//Booleans, with boolean operators as you’d expect.
+func main() {
+	fmt.Println("go"+"lang")
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("7.0/3.0=", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
