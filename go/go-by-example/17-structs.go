eb64fc9e13112350c8ee48eba2f6d2a70eb6c796
faterer <faterer@gmail.com>
2018-06-26 21:02:48 +0800

    u

---
+++ go/go-by-example/17-structs.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+//Go’s structs are typed collections of fields. They’re useful for grouping data together to form records.
+
+type person struct {
+	name string
+	age  int
+}
+
+func main() {
+	fmt.Println(person{"Bob", 20})
+	fmt.Println(person{name: "Alice", age: 30})
+	fmt.Println(person{name: "Fred"})
+	fmt.Println(&person{name: "Ann", age: 40})
+
+	s := person{name: "Sean", age: 50}
+	fmt.Println(s.name)
+
+	sp := &s
+	fmt.Println(sp.age)
+
+	sp.age = 51
+	fmt.Println(sp.age)
+}
